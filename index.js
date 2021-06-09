56aca4d1be4b0c68d9f989c5b861b69b1522bac8
naderabbara <naderabbara@users.noreply.github.com>
2021-06-09 13:08:06 -0400

    bump tailwind version, add other shadows

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ module.exports = plugin(function({ addUtilities, e, theme, variants }) {
       md: '0px 1px 2px rgb(30 29 39 / 19%), 1px 2px 4px rgb(54 64 147 / 18%)',
       lg: '3px 3px 6px rgb(0 0 0 / 26%), 0 0 5px rgb(15 3 86 / 22%)',
       xl: '1px 1px 3px rgb(0 0 0 / 29%), 2px 4px 7px rgb(73 64 125 / 35%)',
+      heavy: '0px 4px 3px rgba(0,0,0,0.4), 0px 8px 13px rgba(0,0,0,0.1), 0px 18px 23px rgba(0,0,0,0.1)',
+      hard: '5px 5px 0px rgba(0,0,0,0.2)',
       none: 'none',
     },
   },
